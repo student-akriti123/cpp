a4650d2d36acaf5532ff54313f3ac0b5c1f030e2
student-akriti123 <akritisinha1602@gmail.com>
2024-05-31 05:17:56 +0530

    first commit

---
+++ code12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int l;
+cout<<"l:";
+cin>>l;
+
+int b;
+cout<<"b:";
+cin>>b;
+
+int area=l*b;
+int perimeter=2*(l+b);
+
+if(area>perimeter)
+    cout<<"area is greater";
+
+    else
+        cout<<"perimeter is greater";
+
+}
