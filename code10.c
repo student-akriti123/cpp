3284ef79a9a9579381e2a9f68c3293a662fa8b99
student-akriti123 <akritisinha1602@gmail.com>
2024-05-30 18:15:32 +0530

    first commit

---
+++ code10.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int sp,cp;
+ cin>>sp,cp;
+
+ if(sp>cp)
+ cout<<"profit";
+
+
+}
---
+++ code11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int sp,cp;
+ cout<<"sp :";
+ cin>> sp;
+ cout<<endl;
+ cout<<"cp :";
+
+ cin>> cp;
+
+ if(sp>cp)
+ cout<<"profit";
+else
+    cout<<"loss";
+
+}
