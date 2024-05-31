157d2069e2eb5e1017b3e45a572f3009215bdc8f
student-akriti123 <akritisinha1602@gmail.com>
2024-05-31 05:39:31 +0530

    first commit

---
+++ code14.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"n:";
+cin>>n;
+if(n%3==0&&n%5==0)
+ cout<<"no. is divisible";
+
+ else
+    cout<<" no. is not divisible";
+
+}
