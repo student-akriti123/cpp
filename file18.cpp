21b08713c3831b67465c19565734536bbfcd5ced
student-akriti123 <akritisinha1602@gmail.com>
2024-07-21 18:39:16 +0530

    fist commit

---
+++ file18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the value of x:";
+cin>>x;
+if(x%2==0)
+{
+cout<<"Even number";
+}
+
+else
+{
+cout<<"odd number";
+}
+return 0;
+}
---
+++ file19.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the value of x:";
+cin>>x;
+
+if(x%5==0)
+{
+cout<<"Divisible by 5";
+}
+else
+{
+cout<<"Not divisible by 5";
+}
+return 0;
+}
