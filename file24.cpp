e06b9e6523e2fd916b6e0692f3f2128556e9751d
student-akriti123 <akritisinha1602@gmail.com>
2024-07-22 05:03:05 +0530

    first commit

---
+++ file24.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cout<<"Enter the value of n:";
+cin>>n;
+
+for(int i=0;i<=n;i++)
+
+{
+
+cout<<i;
+cout<<endl;
+}
+
+
+
+
+return 0;
+}
