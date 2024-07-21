abf92907eaab7af87f860a0392128babf9fa97f7
student-akriti123 <akritisinha1602@gmail.com>
2024-07-21 20:58:36 +0530

    first commit

---
+++ file21.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x;
+cout<<"Enter the value of x:";
+cin>>x;
+
+if(x>99&&x<1000)
+{
+cout<<"It is three digit no.";
+}
+
+else
+{
+cout<<"It is not a three digit no.";
+}
+return 0;
+}
