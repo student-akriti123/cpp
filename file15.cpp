61f9cf70754f5022be064df7fb23ee86f156d087
student-akriti123 <akritisinha1602@gmail.com>
2024-07-21 05:48:41 +0530

    first commit

---
+++ file15.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the value of x:";
+cin>>x;
+int y;
+cout<<"Enter the value of y:";
+cin>>y;
+cout<<"The value of x%y:";
+cout<<x%y;
+return 0;
+}
