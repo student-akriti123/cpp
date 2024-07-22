9d9ae89755bc212522982617c14b206584f9224d
student-akriti123 <akritisinha1602@gmail.com>
2024-07-22 05:40:32 +0530

    first commit

---
+++ file27.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the value of n:";
+cin>>n;
+int a=4
+;
+for(int i=0;i<=n;i=i+1)
+{
+
+cout<<a;
+a=a+3;
+cout<<endl;
+}
+return 0;
+}
