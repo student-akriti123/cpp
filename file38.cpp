6f690f6104cba901962ff953ffb374cfd6896d38
student-akriti123 <akritisinha1602@gmail.com>
2024-07-22 09:03:54 +0530

    first commit

---
+++ file38.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+return a+b;
+}
+
+int main(){
+int a;
+cout<<"Enter the value of a:";
+cin>>a;
+
+int b;
+cout<<"Enter the value of b:";
+cin>>b;
+
+cout<<sum(a,b);
+
+
+return 0;
+}
