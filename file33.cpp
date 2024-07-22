1ecbb02aa4b648e89f1987c2a2738d07886a903d
student-akriti123 <akritisinha1602@gmail.com>
2024-07-22 07:17:17 +0530

    first commit

---
+++ file33.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int m;
+cout<<"Enter the value of m:";
+cin>>m;
+
+int n;
+cout<<"Enter the value of n:";
+cin>>n;
+
+for(int i=1;i<=m;i++)
+{
+for(int j=1;j<=n;j++)
+{
+cout<<i;
+}
+cout<<endl;
+}
+
+return 0;
+}
