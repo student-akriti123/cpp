e517757e87af8c04fc4d3581cdfbf22fd049344b
student-akriti123 <akritisinha1602@gmail.com>
2024-07-22 08:20:51 +0530

    first commit

---
+++ file34.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int m;
+cout<<"Enter the value of m:";
+cin>>m;
+
+int n;
+cout<<"Enter the value of n:";
+cin>>n;
+
+for(int i=1;i<=m;i++)
+{
+for(int j=1;j<=n;j++)
+{
+
+cout<<j;
+}
+cout<<endl;
+}
+
+return 0;
+}
---
+++ file35.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the value of n:";
+cin>>n;
+
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=n;j++)
+{
+cout<< (char)(j+64);
+}
+cout<<" ";
+
+cout<<endl;
+}
+
+return 0;
+}
